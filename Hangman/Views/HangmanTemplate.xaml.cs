13dffbeb42ce0ffff28c68d2c425ea93835bac3b
Aniket Kumar <44815313+aniketk33@users.noreply.github.com>
2021-08-27 20:16:36 +0530

    hangman template created

---
+++ Hangman/Views/HangmanTemplate.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Hangman.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
+
+namespace Hangman.Views
+{
+    public partial class HangmanTemplate : ContentView
+    {
+        public HangmanTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
